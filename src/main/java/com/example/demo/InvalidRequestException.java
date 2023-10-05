a2129507d0d9e70737f9047376de7103e51ea339
Paakhi Iyer <paakhi.iyer@publicissapient.com>
2023-10-05 12:39:17 +0530

    updated commit

---
+++ src/main/java/com/example/demo/InvalidRequestException.java
@@ -0,0 +1,8 @@
+package com.example.demo;
+
+public class InvalidRequestException extends Throwable {
+    public InvalidRequestException(String message) {
+        super(message);
+    }
+}
+
---
+++ src/main/java/com/example/demo/ProjectPaakhi1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectPaakhi1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectPaakhi1Application.class, args);
+	}
+
+}
